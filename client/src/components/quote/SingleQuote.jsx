f6d0086638b30be9dcdc0c1a40d19205cbfae45d
nhrepon <nurhossainrepon7248@gmail.com>
2025-01-23 19:26:44 +0600

    single quote

--- client/src/components/quote/SingleQuote.jsx
+++ client/src/components/quote/SingleQuote.jsx
@@ -26,7 +26,7 @@ const SingleQuote = () => {
             try{
                 const dataUrl = await toPng(cardRef.current, {
                     cacheBust: true,
-                    scale: 2
+                    scale: 1
                 });
 
                 const filename = quote.quote
@@ -49,12 +49,12 @@ const SingleQuote = () => {
     return (
         <div className="container">
 <div className="row justify-content-center my-2">
-    <div className="col-sm-4">
+    <div className="col-sm-3">
         {
             quote && (
-                <div className="card " ref={cardRef}>
-                    <img className="card-img" src={backendUrl+img.filePath} alt={quote.quote} crossOrigin={"anonymous"}/>
-                    <div className="card-body  d-flex align-items-center justify-content-center position-absolute text-center h-100"
+                <div className="card shadow position-relative" ref={cardRef}>
+                    <img className="card-img rounded" src={backendUrl+img.filePath} alt={quote.quote} crossOrigin={"anonymous"}/>
+                    <div className="card-body rounded d-flex align-items-center justify-content-center position-absolute text-center w-100 h-100"
                         style={{backgroundColor: "rgba(0,0,0,0.7)"}}>
                         <div>
                             <span className="d-flex">
@@ -64,7 +64,7 @@ const SingleQuote = () => {
                                     d="M9.563 8.469l-0.813-1.25c-5.625 3.781-8.75 8.375-8.75 12.156 0 3.656 2.688 5.375 4.969 5.375 2.875 0 4.906-2.438 4.906-5 0-2.156-1.375-4-3.219-4.688-0.531-0.188-1.031-0.344-1.031-1.25 0-1.156 0.844-2.875 3.938-5.344zM21.969 8.469l-0.813-1.25c-5.563 3.781-8.75 8.375-8.75 12.156 0 3.656 2.75 5.375 5.031 5.375 2.906 0 4.969-2.438 4.969-5 0-2.156-1.406-4-3.313-4.688-0.531-0.188-1-0.344-1-1.25 0-1.156 0.875-2.875 3.875-5.344z"></path>
                             </svg>
                             </span>
-                            <h2 className="fs-4 card-text text-light text-center mx-3">{quote.quote}</h2>
+                            <p className="card-text text-light text-center mx-2 text-wrap" >{quote.quote}</p>
                             <span className="d-flex justify-content-end">
                                 <svg style={{transform: "rotate(180deg)"}} fill="#fff" width="60px" height="60px" viewBox="0 0 32 32" version="1.1"
                                      xmlns="http://www.w3.org/2000/svg">
