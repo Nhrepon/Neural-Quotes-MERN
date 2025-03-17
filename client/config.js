52697378adfbdfc7d68468b30d3b78262d1a0681
nhrepon <nurhossainrepon7248@gmail.com>
2025-03-17 21:55:56 +0600

    build

--- client/config.js
+++ client/config.js
@@ -1,5 +1,5 @@
 
-export const backendUrl ="http://localhost:2000/";
-//export const backendUrl ="/";
+//export const backendUrl ="http://localhost:2000/";
+export const backendUrl ="/";
 
 export const appName = "Neural Quotes";
